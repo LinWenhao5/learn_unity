5db604adc1c98107e5f218017a7b08968b66b546
Lin Wenhao <linwenhao5@gmail.com>
2023-01-17 14:07:14 +0100

    uitwerking

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            enabled = false;
+            Invoke("Wait", 2f);
+        }
+    }
+    
+    void Wait()
+    {
+        Debug.Log("2 seconds later");
+        enabled = true;
+    }
+}
